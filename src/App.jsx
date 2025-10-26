08b5b5e41c2bde145c187f43ecb1c30d6444abd1
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-10-26 08:41:12 -0300

    feat: add basic nested routes with Home and AppLayout including header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import Home from "./ui/Home";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <RouterProvider router={router}/>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
