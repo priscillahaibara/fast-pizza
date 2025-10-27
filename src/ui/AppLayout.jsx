b3926e88105e6706c643e355db60694d3c2de4c6
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-10-27 09:12:57 -0300

    feat: add loader to AppLayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
       <Header />
 
       <div>
@@ -19,3 +24,5 @@ function AppLayout() {
 }
 
 export default AppLayout;
+
+
