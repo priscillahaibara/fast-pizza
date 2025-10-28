073552b22b69b390bb09e14776bad6a1947e45ea
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-10-28 13:59:46 -0300

    feat: add style to Order

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -57,28 +57,36 @@ function Order() {
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
   return (
-    <div>
-      <div>
-        <h2>Order #{id} status</h2>
+    <div className="space-y-8 px-4 py-6">
+      <div className="flex flex-wrap items-center justify-between gap-2">
+        <h2 className="text-xl font-semibold">Order #{id} status</h2>
 
-        <div>
-          {priority && <span>Priority</span>}
-          <span>order</span>
+        <div className="space-x-2">
+          {priority && (
+            <span className="rounded-full bg-red-500 px-3 py-1 text-sm font-semibold tracking-wide text-red-50 uppercase">
+              Priority
+            </span>
+          )}
+          <span className="rounded-full bg-green-500 px-3 py-1 text-sm font-semibold tracking-wide text-green-50 uppercase">
+            order
+          </span>
         </div>
 
-        <div>
-          <p>
+        <div className="flex flex-wrap items-center justify-between gap-2 bg-stone-200 px-6 py-5">
+          <p className="font-medium">
             {deliveryIn >= 0
               ? `Only ${deliveryIn} minutes left`
               : "Order should have arrived"}
           </p>
-          <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+          <p className="text-xs text-stone-500">
+            (Estimated delivery: {formatDate(estimatedDelivery)})
+          </p>
         </div>
 
-        <div>
-          <p>Price pizza: {formatCurrency(orderPrice)}</p>
+        <div className="space-y-2 bg-stone-200 px-6 py-5">
+          <p className="text-sm font-medium text-stone-600">Price pizza: {formatCurrency(orderPrice)}</p>
           {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-          <p>
+          <p className="font-bold">
             To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
           </p>
         </div>
